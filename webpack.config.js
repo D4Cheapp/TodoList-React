4cc015c11aeda4e66975dd0756a185506c0b1409
Scherbakov Ilya <hitrojopic@gmail.com>
2023-03-18 03:28:28 +0300

    Initial commit
    Комит инициализации проекта с использованием react, redux, webpack, sass, postcss, babel.

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports={
+    //Режим проекта и точка входа
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname,'./src/index.js'),
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    stats: {
+        children: true,
+    },
+    //Настройки сервера
+    devServer: {
+        watchFiles: [path.resolve(__dirname,"src")],
+        historyApiFallback: true,
+        port:3000,
+        hot:true,
+    },
+    //Выходной main файл
+    output: {
+        filename: `[name].js`,
+        path: path.resolve(__dirname,'docs')
+    },
+    resolve: {
+        extensions: ['.ts', '.jsx', '.js', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader",'css-loader']
+            },
+            {
+                //Компиляция из sass в css
+                test: /\.sass$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', {
+                    loader: 'sass-loader',
+                    options: {
+                        //Файл с переменными и миксинами
+                        additionalData: '@import "src/style.sass"',
+                    }},]
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname,'./public/index.html'),
+        favicon: path.resolve(__dirname,'./public/favicon.ico')
+    }),
+        new CleanWebpackPlugin()
+    ]
+}
