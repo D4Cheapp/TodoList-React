8f7a3776ad2054869f990fe5aafd462ae5a03e79
Scherbakov Ilya <hitrojopic@gmail.com>
2023-03-21 19:02:57 +0300

    Refactor task editing
    Добавлено удаление задачи при отсутствии текста.
    Добавлено форматирование текста перед изменением.

--- src/components/TasksContainer.js
+++ src/components/TasksContainer.js
@@ -16,6 +16,8 @@ function TaskCreating({title, checked, id}) {
     const dispatch = useDispatch()
 
     const titleRef = useRef()
+    const checkboxRef = useRef()
+    const deleteButtonRef = useRef()
 
     function deleteTask() {
         dispatch({type: 'DELETE', id: id})
@@ -26,18 +28,33 @@ function TaskCreating({title, checked, id}) {
     }
 
     function taskEditing() {
+        checkboxRef.current.style.display = 'none'
+        deleteButtonRef.current.style.display = 'none'
+
         titleRef.current?.focus()
         titleRef.current?.removeAttribute('readonly')
     }
 
     function focusOut() {
-        dispatch({type:'EDIT', id:id, title:titleRef.current?.value})
-        titleRef.current?.setAttribute('readonly', 'true')
+        const taskValue = titleRef.current?.value.replace(/\s+/gm,' ').trim()
+
+        if (taskValue) {
+            checkboxRef.current.style.display = ''
+            deleteButtonRef.current.style.display = ''
+
+            titleRef.current.value = taskValue
+
+            dispatch({type: 'EDIT', id: id, title: taskValue})
+            titleRef.current?.setAttribute('readonly', 'true')
+        }
+        else {
+            deleteTask()
+        }
     }
 
     return (
         <div className='task' id={id} onDoubleClick={taskEditing} onBlur={focusOut}>
-            <label className='checkbox'>
+            <label className='checkbox' ref={checkboxRef}>
                 <input className='checkbox__input' type='checkbox' checked={checked} onChange={checkTask}/>
 
                 <div className='checkbox__custom'/>
@@ -45,7 +62,7 @@ function TaskCreating({title, checked, id}) {
 
             <textarea className='task__title' ref={titleRef} defaultValue={title} readOnly/>
 
-            <button className='task__remove-button' onClick={deleteTask}/>
+            <button className='task__remove-button' onClick={deleteTask} ref={deleteButtonRef}/>
         </div>
     )
 }
