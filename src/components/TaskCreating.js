f0ea4ca47af4e3ca430c71a25dfd1fcec2599ac2
Scherbakov Ilya <hitrojopic@gmail.com>
2023-03-23 21:39:29 +0300

    Add task styles
    Добавлен файл стилей для задачи и контейнера с задачами

--- src/components/TaskCreating.js
+++ src/components/TaskCreating.js
@@ -1,43 +1,60 @@
 import {useDispatch} from "react-redux";
-import React, {useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
+import Cross from '../images/cross-mark.svg';
 
 function TaskCreating({title, checked, id}) {
     const dispatch = useDispatch()
 
+    const [isChecked, setIsChecked] = useState(false)
+
+    const taskRef = useRef()
     const titleRef = useRef()
     const checkboxRef = useRef()
     const deleteButtonRef = useRef()
+    const inputRef = useRef()
 
     function deleteTask() {
         dispatch({type: 'DELETE', id: id})
     }
 
     function checkTask() {
+        setIsChecked(inputRef.current?.checked)
         dispatch({type: 'CHECK', id: id})
     }
 
     function taskEditing() {
         window.getSelection().removeAllRanges()
-        titleRef.current.selectionStart = titleRef.current.value.length
+        titleRef.current.selectionStart = titleRef.current?.value.length
 
-        checkboxRef.current.style.display = 'none'
-        deleteButtonRef.current.style.display = 'none'
+        taskRef.current?.classList.add('editing')
+        checkboxRef.current.style.visibility = 'hidden'
+        deleteButtonRef.current.style.visibility = 'hidden'
 
         titleRef.current?.focus()
         titleRef.current?.removeAttribute('readonly')
     }
 
+    function resizeTaskTitle() {
+        titleRef.current.style.height = '10px'
+        titleRef.current.style.height = titleRef.current?.scrollHeight+'px'
+    }
+
+    window.addEventListener('resize', resizeTaskTitle)
+
     function editFocusOut() {
         const taskValue = titleRef.current?.value.replace(/\s+/gm,' ').trim()
 
         if (taskValue) {
-            checkboxRef.current.style.display = ''
-            deleteButtonRef.current.style.display = ''
+            taskRef.current?.classList.remove('editing')
+            checkboxRef.current?.removeAttribute('style')
+            deleteButtonRef.current.style.visibility = 'unset'
 
             titleRef.current.value = taskValue
 
             dispatch({type: 'EDIT', id: id, title: taskValue})
             titleRef.current?.setAttribute('readonly', 'true')
+
+            resizeTaskTitle()
         }
         else {
             deleteTask()
@@ -52,18 +69,24 @@ function TaskCreating({title, checked, id}) {
         }
     }
 
+    useEffect(() => {
+        resizeTaskTitle()
+        setIsChecked(inputRef.current?.checked)
+    }, []);
+
     return (
-        <div className='task' id={id} onDoubleClick={taskEditing} onBlur={editFocusOut}>
+        <div className={`task ${isChecked ? 'completed' : ''}`} id={id} onDoubleClick={taskEditing} onBlur={editFocusOut} ref={taskRef}>
             <label className='checkbox' ref={checkboxRef}>
-                <input className='checkbox__input' type='checkbox' checked={checked} onChange={checkTask}/>
+                <input className='checkbox__input' type='checkbox' ref={inputRef}
+                       checked={checked} onChange={checkTask}/>
 
                 <div className='checkbox__custom'/>
             </label>
 
             <textarea className='task__title' ref={titleRef}
-                      onKeyDown={editEnterCheck} defaultValue={title} readOnly/>
+                      onKeyDown={(event) => {editEnterCheck(event); resizeTaskTitle()}} defaultValue={title} readOnly/>
 
-            <button className='task__remove-button' onClick={deleteTask} ref={deleteButtonRef}/>
+            <button className='task__remove-button' style={{backgroundImage: `url(${Cross})`}} onClick={deleteTask} ref={deleteButtonRef}/>
         </div>
     )
 }
